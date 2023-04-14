e7b6b4495b8f321cc8c9832122fc72b56e6a8dfa
Krish <Kris66@gmail.com.com>
2023-04-14 20:57:06 +0100

    Add project files.

---
+++ project for git hub and azure repo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project_for_git_hub_and_azure_repo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
